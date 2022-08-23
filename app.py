afea9b5aa5cc87032cf44faff91bd89a1b12dfa1
Samiruddin Thunder <thunder.cse2018@gmail.com>
2022-08-23 10:30:16 +0530

    tweaked app for heroku

--- app.py
+++ app.py
@@ -11,12 +11,16 @@ class ToDoForm(FlaskForm):
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin123@localhost/tasks'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zvjpkrnjxhmcvr:d4fc13fdd6714df8a8e542db166a677a261e8005990238c473aab79923349c87@ec2-54-86-106-48.compute-1.amazonaws.com:5432/d4ogj54ole3h21'
+
 app.config['SECRET_KEY'] = "my super secret key"
 
 db = SQLAlchemy(app)
 
 class ToDoData(db.Model):
+    __tablename__ = "tasks"
     id = db.Column(db.Integer, primary_key=True)
     todo = db.Column(db.String(200), nullable=False)
     status = db.Column(db.String(200), nullable=False, default="Not Complete") #Complete / Not Complete
